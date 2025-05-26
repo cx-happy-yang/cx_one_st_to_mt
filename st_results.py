9f29850c91de151181124f65f77372b59b1b5286
cx-happy-yang <30431255+cx-happy-yang@users.noreply.github.com>
2025-05-26 17:13:03 +0800

    st_results

---
+++ st_results.py
@@ -0,0 +1,158 @@
+import sqlite3
+from typing import List
+from CheckmarxPythonSDK.CxOne import (
+    get_all_projects,
+    get_branches,
+    get_a_list_of_scans,
+    get_sast_results_by_scan_id,
+)
+
+import logging
+
+# create logger
+logger = logging.getLogger("main")
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+time_stamp_format = "%Y-%m-%dT%H:%M:%S.%fZ"
+
+__all__ = ["logger"]
+
+
+def get_sast_result(project_name: str, branch: str, scan_id: str) -> List[dict]:
+    offset = 0
+    limit = 100
+    page = 1
+    sast_results_collection = get_sast_results_by_scan_id(
+        scan_id=scan_id, offset=offset, limit=limit,
+        sort=["+status", "+severity", "-queryname"]
+    )
+    total_count = int(sast_results_collection.get("totalCount"))
+    sast_results = sast_results_collection.get("results")
+    if total_count > limit:
+        while True:
+            offset = page * limit
+            if offset >= total_count:
+                break
+            sast_results_collection = get_sast_results_by_scan_id(
+                scan_id=scan_id, offset=offset, limit=limit,
+                sort=["+status", "+severity", "-queryname"]
+            )
+            page += 1
+            sast_results.extend(sast_results_collection.get("results"))
+    report_content = []
+    for result in sast_results:
+        if result.state == "TO_VERIFY":
+            continue
+        report_content.append(
+            {
+                "id": None,
+                "project_name": project_name,
+                "branch": branch,
+                "cwe_id": result.cwe_id,
+                "language_name": result.language_name,
+                "query_group": result.query_group,
+                "query": result.query_name,
+                "source_file_name": result.nodes[0].fileName,
+                "source_line": result.nodes[0].line,
+                "source_column": result.nodes[0].column,
+                "source_name": result.nodes[0].fullName,
+                "dest_file_name": result.nodes[-1].fileName,
+                "dest_line": result.nodes[-1].line,
+                "dest_column": result.nodes[-1].column,
+                "dest_name": result.nodes[-1].fullName,
+                "result_state": result.state,
+                "result_severity": result.severity,
+                "comment": "",
+                "similarity_id": result.similarity_id,
+            }
+        )
+    return report_content
+
+
+sql_create_table = """
+CREATE TABLE IF NOT EXISTS results (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+project_name TEXT,
+branch TEXT,
+cwe_id INTEGER,
+language_name TEXT,
+query_group TEXT,
+query TEXT,
+source_file_name TEXT,
+source_line INTEGER,
+source_column INTEGER,
+source_name TEXT,
+dest_file_name TEXT,
+dest_line INTEGER,
+dest_column INTEGER,
+dest_name TEXT,
+result_state TEXT,
+result_severity TEXT,
+comment TEXT,
+similarity_id INTEGER
+);
+"""
+
+sql_create_index = "CREATE INDEX IF NOT EXISTS result_index ON results (project_name, branch)"
+
+sql_insert_table = """
+INSERT INTO results VALUES (
+:id,
+:project_name,
+:branch,
+:cwe_id,
+:language_name,
+:query_group,
+:query,
+:source_file_name,
+:source_line,
+:source_column,
+:source_name,
+:dest_file_name,
+:dest_line,
+:dest_column,
+:dest_name,
+:result_state,
+:result_severity,
+:comment,
+:similarity_id
+)
+"""
+
+
+def insert_into_db(result_data: List[dict]):
+    con = sqlite3.connect("results.db")
+    con.execute(sql_create_table)
+    con.execute(sql_create_index)
+    try:
+        with con:
+            con.executemany(sql_insert_table, result_data)
+    except sqlite3.IntegrityError:
+        print("couldn't add data twice")
+
+    # Connection object used as context manager only commits or rollbacks transactions,
+    # so the connection object should be closed manually
+    con.close()
+
+
+if __name__ == '__main__':
+    projects = get_all_projects()
+    for project in projects:
+        project_id = project.id
+        project_name = project.name
+        branches = get_branches(limit=2048, project_id=project_id)
+        if not branches:
+            continue
+        for branch in branches:
+            scans_collection = get_a_list_of_scans(limit=1, project_id=project_id, branch=branch)
+            if not scans_collection.scans:
+                continue
+            scan_id = scans_collection.scans[0].id
+            scan_result = get_sast_result(project_name, branch, scan_id)
+            if not scan_result:
+                continue
+            insert_into_db(scan_result)
